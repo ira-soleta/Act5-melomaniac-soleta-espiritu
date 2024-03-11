0cf26ca011d8cd7d98c0afeb358d203f7aa0b6a0
SoletaIrakristine <irakristinesoleta@gmail.com>
2024-03-11 22:11:55 +0800

    song 3 commited by Soleta

---
+++ Espiritu-php-file/song3.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raining in Manila</title>
+</head>
+<body>
+   <h2>Raining in Manila</h2>
+   <h4>Artist: Lola Amour</h4>
+
+   <?php
+       echo"Lyrics: <br><br>";
+       echo" It's been raining in Manila, hindi ka ba nilalamig?<br>";   
+       echo" It's been raining in Manila, hindi ka ba nilalamig?<br>";
+       echo" But, if it's raining in Manila, hindi kita maririnig<br>";
+       echo" So, I'll be waiting in Manila kahit di ka na babalik<br>";
+       echo" <br>";
+       echo" Maulan ba sa inyo 'pag bumubuhos dito?<br>";
+       echo" Paumanhin at mukhang hindi ko<br>";
+       echo" Masasabayan ang 'yong yapak<br>";
+       echo" Sa pagngiti at pag-iyak<br>";
+       echo" Sa paglipad at pagbagsak ng araw-araw<br>";
+       echo" Sa pagpikit na lang kita<br>";
+       echo" Matititigan sa mata<br>";
+       echo" Sa panaginip na magpapaligaw<br>";
+       echo" <br>";
+       echo" Kamusta ka na? Kahit 'wag nang sagutin<br>";
+       echo" 'Di ba nawala ang kintab ng bituin?<br>";
+       echo" Sana ganun ka nga pa rin<br>";
+       echo" <br>";
+       echo" 'Cause, it's been raining in Manila, hindi ka ba nilalamig?<br>";
+       echo" Mahirap bang mag-isang nanginginig?<br>";
+       echo" And it's been raining in Manila, hindi ka ba nilalamig<br>";
+       echo" 'Pag wala ang mga tala?<br>";
+       echo" Madilim ba ang mundo?<br>";
+       echo" <br>";
+       echo" May kulang ba sa inyo na naiwan dito?<br>";
+       echo" Aanhin ang ulan sa paradiso?<br>";
+       echo" Sakali madulas ay dati malapit ka<br>";
+       echo" Ngayon walang kahati ng init 'pag maulan<br>";
+       echo" Sana naman tumigil na ang ulan<br>";
+       echo" <br>";
+       echo" Kamusta ka na? Kahit 'wag nang sagutin<br>";
+       echo" 'Di ba nawala ang kintab ng bituin?<br>";
+       echo" Sana ganun ka nga pa rin<br>";
+       echo" <br>";
+       echo" 'Cause, it's been raining in Manila, hindi ka ba nilalamig?<br>";
+       echo" Mahirap bang mag-isang nanginginig?<br>";
+       echo" And it's been raining in Manila, hindi ka ba nilalamig<br>";
+       echo" 'Pag wala ang mga tala?<br>";
+       echo" Madilim ba ang mundo?<br>";
+       echo" <br>";
+       echo" Kamusta ka na? Kahit 'wag nang sagutin<br>";
+       echo" 'Di ba nawala ang kintab ng bituin?<br>";
+       echo" Sana ganun ka nga pa rin<br>";
+       echo" <br>";
+       echo" But, if it's raining in Manila, hindi kita maririnig<br>";
+       echo" Nakahiga, mag-isang nanginginig<br>";
+       echo" So, I'll be waiting in Manila kahit 'di ka na babalik<br>";
+       echo" Andiyan lang ang mga tala, andiyan lang ang mga tala<br>";
+       echo" Saan mang sulok ng mundo<br>";
+       echo" <br>";
+   ?>
+
+   <a href="index.php">Home</a>
+</body>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</html>

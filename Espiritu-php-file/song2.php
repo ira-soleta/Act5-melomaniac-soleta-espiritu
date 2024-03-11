470aa9e3353b2a34f7da90b926f784b338c2befa
SoletaIrakristine <irakristinesoleta@gmail.com>
2024-03-11 22:11:27 +0800

    song 2 commited by Soleta

---
+++ Espiritu-php-file/song2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kisame</title>
+</head>
+<body>
+   <h2>Kisame</h2>
+   <h4>Artist: Rhodessa</h4>
+
+   <?php
+       echo"Lyrics: <br><br>";
+       echo" Gustong tumingin<br>";   
+       echo" Sa iyong mga mata<br>"; 
+       echo" Na nagniningning<br>"; 
+       echo" Kahit nasaan ka pa<br>"; 
+       echo" <br>"; 
+       echo" Pero bakit ba<br>"; 
+       echo" Ang layo mo<br>"; 
+       echo" Laging magkabilang mundo<br>"; 
+       echo" <br>"; 
+       echo" Pero bakit ba<br>"; 
+       echo" Ang layo mo<br>"; 
+       echo" Pwedeng sa ‘yo na lang ako<br>";
+       echo" <br>";
+       echo" Nakatitig sa kisame<br>";
+       echo" Kakaisip kung pa’no sasabihin sa iyo<br>";
+       echo" Na gusto<br>";
+       echo" Gustong-gusto kita<br>";
+       echo" <br>";
+       echo" Hindi na kita<br>";
+       echo" Maalis sa isip ko<br>";
+       echo" ‘Di maikaila<br>";
+       echo" Na ikaw ang tahanan ko<br>";
+       echo" <br>";
+       echo" Pero bakit ba<br>";
+       echo" Lumalayo<br>";
+       echo" Palagi na lang ba ganito<br>";
+       echo" <br>";
+       echo" Pero bakit ba<br>";
+       echo" Ayaw mo<br>";
+       echo" Pwedeng sa ‘yo na lang ako<br>";
+       echo" <br>";
+       echo" Nakatitig sa kisame<br>";
+       echo" Kakaisip kung pa’no sasabihin sa iyo<br>";
+       echo" Na gusto<br>";
+       echo" Gustong-gusto kita<br>";
+       echo" <br>";
+       echo" Okay lang kahit hindi mo ako pansinin<br>";
+       echo" Kaya ko naman damhin lahat ng sakit<br>";
+       echo" Basta ikaw<br>";
+       echo" <br>";
+       echo" Okay lang kahit hindi mo ako pansinin<br>";
+       echo" Kaya ko naman damhin lahat ng sakit<br>";
+       echo" Basta ikaw<br>";
+       echo" <br>";
+       echo" Nakatitig sa kisame<br>";
+       echo" Kakaisip kung pa’no sasabihin sa iyo<br>";
+       echo" Na gusto<br>";
+       echo" Gustong-gusto kita<br>";
+       echo" <br>";
+       echo" Nakatitig sa kisame<br>";
+       echo" Kakaisip kung pa’no sasabihin sa iyo<br>";
+       echo" Na gusto<br>";
+       echo" Gustong-gusto kita<br>";  
+   ?>
+
+   <a href="index.php">Home</a>
+</body>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</html>

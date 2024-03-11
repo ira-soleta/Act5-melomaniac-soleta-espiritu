62c8d5c2a7472f4d70136d3e57b9e3753c2e15a5
SoletaIrakristine <irakristinesoleta@gmail.com>
2024-03-11 22:10:07 +0800

    index commited by Soleta

---
+++ Espiritu-php-file/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+</head>
+<body>
+    <h1>My Song Book</h1>
+    <ol>
+        <li><a href="song1.php">Jopay</a></li>
+        <li><a href="song2.php">Kisame</a></li>
+        <li><a href="song3.php">Raining In Manila</a></li>
+        <li><a href="song4.php">Story of My Life</a></li>
+        <li><a href="song5.php">Traitor</a></li>
+    </ol>
+</body>
+
+    <style>
+        body{
+            margin: 35px auto;
+           
+        }
+    </style>
+</html>

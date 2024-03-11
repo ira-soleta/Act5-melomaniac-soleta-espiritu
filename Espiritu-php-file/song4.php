e5d57601bd1c4e91caed2886bb04bda54a10c32f
SoletaIrakristine <irakristinesoleta@gmail.com>
2024-03-11 22:12:24 +0800

    song 4 commited by Soleta

---
+++ Espiritu-php-file/song4.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Story of My Life</title>
+</head>
+<body>
+   <h2>Story of My Life</h2>
+   <h4>Artist: One Direction</h4>
+
+   <?php
+       echo"Lyrics: <br><br>";
+       echo" Written in these walls are the stories that I can't explain<br>";  
+       echo" I leave my heart open but it stays right here empty for days<br>";
+       echo" She told me in the morning she don't feel the same about us in her bones<br>";
+       echo" It seems to me that when I die these words will be written on my stone<br>";
+       echo" <br>";
+       echo" And I'll be gone, gone tonight<br>";
+       echo" The ground beneath my feet is open wide<br>";
+       echo" The way that I been holding on too tight<br>";
+       echo" With nothing in between<br>";
+       echo" <br>"; 
+       echo" The story of my life<br>";
+       echo" I take her home<br>";
+       echo" I drive all night<br>";
+       echo" To keep her warm<br>";
+       echo" And time is frozen (the story of, the story of)<br>";
+       echo" The story of my life<br>";
+       echo" I give her hope<br>";
+       echo" I spend her love<br>";
+       echo" Until she's broke<br>";
+       echo" Inside<br>";
+       echo" The story of my life (the story of, the story of)<br>";
+       echo" <br>";
+       echo" Written on these walls are the colors that I can't change<br>";
+       echo" Leave my heart open but it stays right here in its cage<br>";
+       echo" I know that in the morning now I'll see us in the light upon a hill<br>";
+       echo" Although I am broken, my heart is untamed, still<br>";
+       echo" <br>";
+       echo" And I'll be gone, gone tonight<br>";
+       echo" The fire beneath my feet is burning bright<br>";
+       echo" The way that I've been holding on so tight<br>";
+       echo" With nothing in between<br>";
+       echo" <br>";
+       echo" The story of my life<br>";
+       echo" I take her home<br>";
+       echo" I drive all night<br>";
+       echo" To keep her warm<br>";
+       echo" And time is frozen (the story of, the story of)<br>";
+       echo" The story of my life<br>";
+       echo" I give her hope<br>";
+       echo" I spend her love<br>";
+       echo" Until she's broke<br>";
+       echo" Inside<br>";
+       echo" The story of my life (the story of, the story of)<br>";
+       echo" <br>";
+       echo" And I been waiting for this time to come around<br>";
+       echo" But baby running after you is like chasing the clouds<br>";
+       echo" <br>";
+       echo" The story of my life<br>";
+       echo" I take her home<br>";
+       echo" I drive all night<br>";
+       echo" To keep her warm<br>";
+       echo" And time is frozen<br>";
+       echo" The story of my life<br>";
+       echo" I give her hope (give her hope)<br>";
+       echo" I spend her love<br>";
+       echo" Until she's broke (until she's broke inside)<br>";
+       echo" Inside<br>";
+       echo" The story of my life (the story of, the story of)<br>";
+       echo" <br>";
+       echo" The story of my life<br>";
+       echo" The story of my life (the story of, the story of)<br>";
+       echo" The story of my life<br>";
+       echo" <br>";
+   ?>
+
+   <a href="index.php">Home</a>
+</body>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</html>

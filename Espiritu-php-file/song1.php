5598edd7c003369f9dc5e1513d9c4326a256787d
SoletaIrakristine <irakristinesoleta@gmail.com>
2024-03-11 22:11:00 +0800

    song 1 commited by Soleta

---
+++ Espiritu-php-file/song1.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jopay</title>
+</head>
+<body>
+   <h2>Jopay</h2>
+   <h4>Artist: Mayonnaise</h4>
+
+   <?php
+       echo"Lyrics: <br><br>";
+       echo"Jopay, kamusta ka na? <br>";  
+       echo"Palagi kita'ng pinapanood nakikita <br>";
+       echo" Jopay, pasensya ka na<br>";
+       echo" Wala rin kasi akong makausap at kasama<br>";
+       echo" <br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo"'Wag ka ng mawala... <br>";
+       echo" Ngayon<br>";
+       echo" <br>";
+       echo" Dadalhin kita sa aming bahay <br>";
+       echo" 'Di tayo magaaway<br>";
+       echo" Aalis tayo sa tunay na mundo <br>";
+       echo" <br>";
+       echo" Dadalhin kita sa aming bahay <br>";
+       echo" 'Di tayo magaaway <br>";
+       echo" Aalis tayo sa tunay na mundo <br>";
+       echo" <br>";
+       echo" Jopay, kamusta na ba?<br>";
+       echo" Buti ka pa, palagi ka'ng masaya<br>";
+       echo" Jopay, buti na lang<br>";
+       echo" Nariyan ka, hindi na ako nagiisa<br>";
+       echo" <br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" Ngayon<br>";
+       echo" <br>";
+       echo" Dadalhin kita sa aming bahay<br>"; 
+       echo" 'Di tayo magaaway<br>";
+       echo" Aalis tayo sa tunay na mundo<br>";
+       echo" <br>";
+       echo" Dadalhin kita sa aming bahay<br>";
+       echo" 'Di tayo magaaway<br>";
+       echo" Aalis tayo sa tunay na mundo<br>";
+       echo" <br>";
+       echo" Sa tunay na mundo<br>";
+       echo" <br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" 'Wag ka ng mawala...<br>";
+       echo" Ngayon<br>";
+       echo" <br>";
+       echo" Dadalhin kita sa aming bahay<br>";
+       echo" 'Di tayo magaaway<br>";
+       echo" Aalis tayo sa tunay na mundo<br>";
+       echo" <br>";
+       echo" Dadalhin kita sa aming bahay<br>";
+       echo" 'Di tayo magaaway<br>";
+       echo" Aalis tayo sa tunay na mundo<br>";
+       echo" <br>";
+       echo" Sa tunay na mundo<br>";
+       echo" <br>";
+   ?>
+
+   <a href="index.php">Home</a>
+</body>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</html>

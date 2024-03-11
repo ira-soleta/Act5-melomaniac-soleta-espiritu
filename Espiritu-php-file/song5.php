4fa28486c81b2c55b2e7ae9671ac48c97448bf75
SoletaIrakristine <irakristinesoleta@gmail.com>
2024-03-11 22:12:51 +0800

    song 5 commited by Soleta

---
+++ Espiritu-php-file/song5.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Traitor</title>
+</head>
+<body>
+   <h2>Traitor</h2>
+   <h4>Artist: Olivia Rodrigo</h4>
+
+   <?php
+       echo"Lyrics: <br><br>";
+       echo" Brown guilty eyes and little white lies<br>";   
+       echo" Yeah, I played dumb but I always knew<br>";
+       echo" That you talked to her, maybe did even worse<br>";
+       echo" I kept quiet so I could keep you<br>";
+       echo" <br>";
+       echo" And ain't it funny<br>";
+       echo" How you ran to her<br>";
+       echo" The second that we called it quits?<br>";
+       echo" <br>";
+       echo" And ain't it funny<br>";
+       echo" How you said you were friends?<br>";
+       echo" Now it sure as hell don't look like it<br>";
+       echo" <br>";
+       echo" You betrayed me<br>";
+       echo" And I know that you'll never feel sorry<br>";
+       echo" For the way I hurt, yeah<br>";
+       echo" <br>";
+       echo" You talked to her<br>";
+       echo" When we were together<br>";
+       echo" Loved you at your worst<br>";
+       echo" But that didn't matter<br>";
+       echo" <br>";
+       echo" It took you two weeks<br>";
+       echo" To go off and date her<br>";
+       echo" Guess you didn't cheat<br>";
+       echo" But you're still a traitor<br>";
+       echo" <br>";
+       echo" Now you bring her around<br>";
+       echo" Just to shut me down<br>";
+       echo" Show her off like she's a new trophy<br>";
+       echo" <br>";
+       echo" And I know if you were true<br>";
+       echo" There's no damn way that you<br>";
+       echo" Could fall in love with somebody that quickly<br>";
+       echo" <br>";
+       echo" Ain't it funny<br>";
+       echo" All the twisted games<br>";
+       echo" All the questions you used to avoid?<br>";
+       echo" <br>";
+       echo" Ain't it funny?<br>";
+       echo" Remember I brought her up<br>";
+       echo" And you told me I was paranoid<br>";
+       echo" <br>";
+       echo" You betrayed me<br>";
+       echo" And I know that you'll never feel sorry<br>";
+       echo" For the way I hurt, yeah<br>";
+       echo" <br>";
+       echo" You talked to her<br>";
+       echo" When we were together<br>";
+       echo" Loved you at your worst<br>";
+       echo" But that didn't matter<br>";
+       echo" <br>";
+       echo" It took you two weeks<br>";
+       echo" To go off and date her<br>";
+       echo" Guess you didn't cheat<br>";
+       echo" But you're still a traitor<br>";
+       echo" <br>";
+       echo" God, I wish that you had thought this through<br>";
+       echo" Before I went and fell in love with you<br>";
+       echo" When she's sleeping in the bed we made<br>";
+       echo" Don't you dare forget about the way<br>";
+       echo" <br>";
+       echo" You betrayed me<br>";
+       echo" 'Cause I know that you'll never feel sorry<br>";
+       echo" For the way I hurt, yeah<br>";
+       echo" <br>";
+       echo" You talked to her<br>";
+       echo" When we were together<br>";
+       echo" You gave me your word<br>";
+       echo" But that didn't matter<br>";
+       echo" <br>";
+       echo" It took you two weeks<br>";
+       echo" To go off and date her<br>";
+       echo" Guess you didn't cheat<br>";
+       echo" But you're still<br>";
+       echo" <br>";
+       echo" You're still a traitor<br>";
+       echo" Yeah, you're still a traitor<br>";
+       echo" <br>";
+       echo" God, I wish that you had thought this through<br>";
+       echo" Before I went and fell in love with you<br>";
+       echo" <br>";
+
+   ?>
+
+   <a href="index.php">Home</a>
+</body>
+    <style>
+        body{
+            text-align: center;
+        }
+    </style>
+</html>
